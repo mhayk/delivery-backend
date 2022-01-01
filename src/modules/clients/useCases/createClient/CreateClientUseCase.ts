56258d5f23d398e78bd50ae23ccd397baf0e9e91
Mhayk Whandson <hi@mhayk.com>
2022-01-01 07:03:43 +0000

    create client use case has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from 'bcrypt';
+
+interface ICreateClient {
+    username: string;
+    password: string;
+}
+
+export class CreateClientUseCase {
+    async execute({ username, password }: ICreateClient) {
+        // validate if client exists
+        const clientExists = await prisma.clients.findFirst({
+            where: {
+                username: {
+                    mode: 'insensitive'
+                }
+            }
+        });
+
+        if (clientExists) {
+            throw new Error('Client already exists');
+        }
+
+        // crypt password
+        const hashedPassword = await hash(password, 10);
+
+        // create client
+        const newClient = await prisma.clients.create({
+            data: {
+                username,
+                password: hashedPassword,
+            }
+        });
+
+        return newClient
+    }
+}

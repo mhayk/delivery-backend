78bd90845ab309c60696f25871cca493f48dc260
Mhayk Whandson <hi@mhayk.com>
2022-01-02 14:06:47 +0000

    a middleware to handle error via exeptions has been added.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import "express-async-errors"
 import { routes } from './routes'
 
 const app = express()
@@ -7,9 +8,16 @@ app.use(express.json())
 
 app.use(routes)
 
-app.get('/', (request, response) => {
-    return response.json({
-        msg: "Hello world !"
+app.use((err: Error, request: Request, response: Response, nextFunction: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: 'Internal server error'
     })
 })
 
